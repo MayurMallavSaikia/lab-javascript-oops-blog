b3dd3f7b6295ea925c715e668cc9ae1c062a7f92
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-07-14 15:42:09 +0530

    Mayur Mallav Saikia

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 /* Fill your code*/
+document.getElementById('addBlog').addEventListener('click',function(){
+    document.getElementById('popupContact').style.display="block";
+})
+
+document.getElementById('close').addEventListener('click',function(){
+    document.getElementById('popupContact').style.display="none";
+})
+
+
+
+
+
+class Blog
+{
+    constructor(title,detail)
+    {
+        this.title=title;
+        this.detail=detail;
+    }
+
+    addImage(){
+
+        var image=document.createElement('img');
+        image.src="assets/javascript.png";
+        document.getElementById("card-text").appendChild(image);
+    }
+
+
+
+    addTitle()
+    {
+        var title_card = document.createElement('h1');
+        title_card.setAttribute("id","blog-title");
+        document.getElementById('card-text').appendChild(title_card);
+        title_card.innerHTML += this.title;
+
+    }
+    addDescription()
+    {
+        var display = document.createElement('p');
+        display.setAttribute("id","blog-description");
+        document.getElementById('card-text').appendChild(display);
+        display.innerHTML += this.detail+"<br><br>";
+    }
+}
+
+var post=document.getElementById('post');
+post.addEventListener('click',function(){
+ 
+    var title=document.getElementById('title').value;
+    var detail=document.getElementById('detail').value;
+
+    var add_blog = new Blog(title,detail);
+    add_blog.addImage();
+    add_blog.addTitle();
+    add_blog.addDescription();
+
+})
